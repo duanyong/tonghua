c3eb9f938378f48d7fe947396b0980fe541fba14
duanyong <coderduan@gmail.com>
2017-01-11 16:35:04 +0800

    修改gruntfiles.js配置文件

--- crm/web/WEB-ROOT/Gruntfile.js
+++ crm/web/WEB-ROOT/Gruntfile.js
@@ -2,37 +2,63 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        //合并文件
         concat: {
             options: {
+                // 定义一个用于插入合并输出文件之间的字符
                 separator: ';'
             },
-            dist: {
-                src: ['src/**/*.js'],
-                dest: 'dist/<%= pkg.name %>.js'
+
+            //合并JS文件
+            jsconcat: {
+                // 将要被合并的文件
+                src: ['js/*.js'],
+                // 合并后的JS文件的存放位置
+                dest: 'release/<%= pkg.name %>.one.js'
+            },
+
+            //合并css文件
+            cssconcat: {
+                // 将要被合并的文件
+                src: ['css/*.css'],
+                // 合并后的JS文件的存放位置
+                dest: 'release/<%= pkg.name %>.one.css'
             }
         },
+
+        //压缩（minify）JavaScript文件
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
             },
-            dist: {
+
+            jsconcat: {
                 files: {
-                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                    'release/<%= pkg.name %>.min.js': ['<%= jsconcat.dist.dest %>']
+                }
+            },
+
+            cssconcat: {
+                files: {
+                    'release/<%= pkg.name %>.min.css': ['<%= cssconcat.dist.dest %>']
                 }
             }
         },
         qunit: {
             files: ['test/**/*.html']
         },
+
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            files: ['Gruntfile.js', 'js/*.js', 'css/*.css'],
             options: {
                 // options here to override JSHint defaults
+                jshintrc        : '.jshintrc',
+                reporterOutput  : '',
                 globals: {
-                    jQuery: true,
-                    console: true,
-                    module: true,
-                    document: true
+                    jQuery      : true,
+                    console     : true,
+                    module      : true,
+                    document    : true
                 }
             }
         },
